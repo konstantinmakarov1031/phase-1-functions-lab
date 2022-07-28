3b2090598b530211bda167c6379270eb71403d1d
konstantin <makarovestates@gmail.com>
2022-07-28 05:31:22 -0500

    commit tree d43b72b7f962a9d83ecd778224bd1c9f523456c9

--- index.js
+++ index.js
@@ -44,25 +44,27 @@ const distanceTravelledInFeet = function(startpoint, endpoint){
 
 
 const calculatesFarePrice = function(startpoint, endpoint){
+
+    let distance = distanceTravelledInFeet(startpoint, endpoint) 
+
+    if (distance < 400)
+        {return 0};
     
-    if (((endpoint - startpoint) * 264) < 400)
-        return 0; 
+    if (distance >= 400 && distance <= 2000)
+        {return ((distance - 400) * 0.02)};
 
-    else if ((((endpoint - startpoint) * 264) >= 400) 
-    && (((endpoint - startpoint) * 264) <= 2000)) 
-        return (((endpoint - startpoint) * 5.28) - 8);
+    if (distance > 2000 && distance <= 2500) 
+        {return 25};
+    
+    if(distance > 2500)
+        {return "cannot travel that far"}};
 
-    else if ((((endpoint - startpoint) * -264) >= 400) 
-    && (((endpoint - startpoint) * -264) <= 2000)) 
-        return (((endpoint - startpoint) * 5.28) - 8);
 
-    else if (((endpoint - startpoint) * 264) > 2000)    
-        return 25;
-
-    else if (((endpoint - startpoint) * 264) > 2500) 
-        return 'cannot travel that far';
-
-}
-
-            calculatesFarePrice(34,32)
-
+   
+// `calculatesFarePrice`: Given the same starting and ending block as the
+// previous test (_hint hint_), return the fare for the customer. The first four
+// hundred feet are free. For a distance between 400 and 2000 feet, the price is
+// 2 cents per foot (not including 400, which are free!). Then Scuber charges a
+// flat fare for a distance over 2000 feet and under 2500 feet.
+// Finally, Scuber does not allow any rides over 2500 feet — the function returns
+// `'cannot travel that far'` if a ride over 2500 feet is requested.
